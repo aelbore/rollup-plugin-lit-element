68aae56e8194f1e1ce20869b0383ee01bca161dd
Arjay Elbore <arjay.elbore@bettertradeoff.com>
2022-10-27 13:50:37 +0800

    add external and types

--- qoi.config.ts
+++ qoi.config.ts
@@ -1,17 +1,18 @@
 import { existsSync } from 'fs'
 import { rename, rm } from 'fs/promises'
+import { defineConfig } from 'qoi-cli'
 
-import { defineConfig } from '@qoi/build'
-
+import { compilerOptions } from './tsconfig.json'
+ 
 export default defineConfig({
   swc: {
     jsc: {
-      target: 'es2022'
+      target: 'es2022',
+      paths: compilerOptions.paths
     }
   },
-  async onBuildEnd() {
+  async buildEnd() {
     const outDir = './node_modules/rollup-plugin-lit-element'
-
     existsSync(outDir) && await rm(outDir, { recursive: true }) 
     await rename('./dist', outDir)
   }
